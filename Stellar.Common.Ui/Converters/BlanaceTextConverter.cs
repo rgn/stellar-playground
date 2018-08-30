1054d3400878dfca26de8a2c7ba21bc538849082
tvdgnr <ralf.gnaedinger@trivadis.com>
2018-08-30 21:22:12 +0200

    Fix display issues for account and payments.

---
+++ Stellar.Common.Ui/Converters/BlanaceTextConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Stellar.Common.Ui.Converters
+{
+    public class BlanaceTextConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var balanceType = values[0] as string;            
+            var balanceValue = values[1] as string;
+
+            return $"{balanceType} {balanceValue}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
